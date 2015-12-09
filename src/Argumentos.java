54ca5c79a3e33acb87c182e0ccdf966a8be3eb5c
EDUARDO BAUER <edu.bauer1@gmail.com>
2015-12-09 02:00:27 -0200

    Argumentos #2
    observação

--- src/Argumentos.java
+++ src/Argumentos.java
@@ -3,7 +3,7 @@ class Argumentos {
 
 	public static void main(String[] args){
 
-		System.out.println ("Voce digitou" + args[0]);
+		System.out.println ("Voce digitou" + args[0]); //necessário inserir o texto de argumento ao rodar o programa
 
 	}
 
