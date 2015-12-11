b20925e742181162bdd2957b001d5cb3fdc30a4f
EDUARDO BAUER <edu.bauer1@gmail.com>
2015-12-11 01:31:52 -0200

    First Commit

---
+++ src/com/loiane/cursojava/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+
+		double ValorPassagem = 3.20;
+		
+		float ValorTomate = 3.95f;
+		
+		System.out.println("Valor da Passagem = " + ValorPassagem);
+		
+		System.out.println("Valor do Tomate = " + ValorTomate);
+				
+
+	}
+
+}
