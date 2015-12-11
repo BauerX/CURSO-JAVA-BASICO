d3952315a6a16e0d56925bef914635a8e6ee9745
EDUARDO BAUER <edu.bauer1@gmail.com>
2015-12-11 00:36:38 -0200

    First Commit
    variáveis

---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		
+		String nome="Eduardo";
+		
+		idade = 25;
+		
+		System.out.println("nome = " + nome);
+	
+
+	}
+
+}
