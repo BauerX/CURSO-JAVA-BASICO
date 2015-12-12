cd168cc7966dd0cc249b933cd156cff9e1b82f32
EDUARDO BAUER <edu.bauer1@gmail.com>
2015-12-12 19:51:21 -0200

    First Commit
    Objetivo, inserir SIM e entender como true

---
+++ src/com/loiane/cursojava/aula12/TesteBoolean.java
@@ -0,0 +1,22 @@
+package com.loiane.cursojava.aula12;
+
+import java.util.Scanner;
+
+public class TesteBoolean {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Você usa óculos?: ");
+		boolean usaOculos  = scan.nextBoolean();
+			
+		//objetivo inserir Sim na resposta e o programa entender True, se der certo a resposta será OK	
+	
+		System.out.println("ok");
+	     
+		
+	}
+
+}
+
